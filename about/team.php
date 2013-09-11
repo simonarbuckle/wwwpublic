75d0174d56a1aef948548addebf60fff81e66177
okappes <okappes@amadeus.com>
2013-09-11 11:45:22 +0200

    Updated team page

--- about/team.php
+++ about/team.php
@@ -11,145 +11,146 @@
 					<div class="columns main-page two">
 						<article class="column">
 							<h1>People who make it</h1>
-							
 							<div class="team">
-							
-								<h3>Core team</h3>
-								<div class="member">
-									<h5>Patrick Brosset</h5>
-									<img src="https://secure.gravatar.com/avatar/7040523d5fdaa01072dd86d5e6dc0d0a?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/captainbrosset">captainbrosset</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
-								<div class="member">
-									<h5>Fabio Crisci</h5>
-									<img src="https://secure.gravatar.com/avatar/c36dfb36e0929fe44887929a88bc5fb2?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/piuccio">piuccio</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
-								<div class="member">
-									<h5>David-Emmanuel Divernois</h5>
-									<img src="https://secure.gravatar.com/avatar/a7a83041f787ea36c9c3479d15f279b9?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/divdavem">divdavem</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
-								<div class="member">
-									<h5>Francesco Longo</h5>
-									<img src="https://secure.gravatar.com/avatar/f045cf96a82ac30d9d92c663b4828e00?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/flongo">flongo</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
+								<h3>Core contributors</h3>
 								<div class="member">
 									<h5>Simon Arbuckle</h5>
-									<img src="https://secure.gravatar.com/avatar/1adb30759cd952d126999c246d89661b?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
+									<img src="https://0.gravatar.com/avatar/c61867b2c23e5a5480d25fe9cbd10b44?d=https%3A%2F%2Fidenticons.github.com%2Fa0262269bf1b15690a159743343c00be.png&s=420" />
 									<div class="github"><a href="https://github.com/simonarbuckle">simonarbuckle</a></div>
 									<div class="location">Nice, France</div>
 								</div>
-								
 								<div class="member">
 									<h5>Fabrice Basso</h5>
-									<img src="https://secure.gravatar.com/avatar/6aab531fcdf3f9af34a333e10ec7b3b2?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
+									<img src="https://1.gravatar.com/avatar/6aab531fcdf3f9af34a333e10ec7b3b2?d=https%3A%2F%2Fidenticons.github.com%2Ff881b3431866f796b031b68212e416b5.png&s=420" />
 									<div class="github"><a href="https://github.com/fbasso">fbasso</a></div>
 									<div class="location">Nice, France</div>
 								</div>
-								
 								<div class="member">
-									<h5>Jakub Gieryluk</h5>
-									<img src="https://secure.gravatar.com/avatar/96246ecff3f790f6b8a35c80c6f67ea3?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/jakub-g">jakub-g</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
-								<div class="member">
-									<h5>Jineesh John</h5>
-									<img src="https://secure.gravatar.com/avatar/a5c78f40d8784234513c7dff9b19be1c?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/jineeshjohn">jineeshjohn</a></div>
+									<h5>Susanta Behera</h5>
+									<img src="https://1.gravatar.com/avatar/20aa3a9f39711058135a38016ca75b0d?d=https%3A%2F%2Fidenticons.github.com%2F68015967da9b4a3299386916466ddacb.png&s=420" />
+									<div class="github"><a href="https://github.com/susant123">susant123</a></div>
 									<div class="location">Bangalore, India</div>
 								</div>
-								
-								<div class="member">
-									<h5>Anand Chandrabhanu</h5>
-									<img src="https://secure.gravatar.com/avatar/011ebf4ff9918d776805e3f063a7384c?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/anandc">anandc</a></div>
-									<div class="location">Bangalore, India</div>
-								</div>
-								
-								<div class="member">
-									<h5>Girish Prasad</h5>
-									<img src="https://secure.gravatar.com/avatar/cb7e0f1c2a70bd8fe7af432260cf982c?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/gprasad">gprasad</a></div>
-									<div class="location">Bangalore, India</div>
-								</div>
-							</div>
-							
-							<div class="team">
-								<h3>Documentation team</h3>
-								<div class="member">
-									<h5>Olaf Kappes</h5>
-									<img src="https://secure.gravatar.com/avatar/ce3e0e6ff44f08595dffa086c81e005d?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
-									<div class="github"><a href="https://github.com/olaf-k">olaf-k</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
-								<div class="member">
-									<h5>Benoit Charbonnier</h5>
-									<img src="https://secure.gravatar.com/avatar/74b3575da062a5bdc1e47b7853897d55?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png"/>
-									<div class="github"><a href="https://github.com/benouat">benouat</a></div>
-									<div class="location">Nice, France</div>
-								</div>
-								
 								<div class="member">
 									<h5>Fabiano Bernardo</h5>
-									<img src="https://secure.gravatar.com/avatar/4212d917a8047a1ddb72f01a75de1219?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png" />
+									<img src="https://0.gravatar.com/avatar/5b2e0ee2c986e068b254379d8dbfded0?d=https%3A%2F%2Fidenticons.github.com%2Fa20597f08ea792d6c3375028dfbb66c7.png&s=420" />
 									<div class="github"><a href="https://github.com/fab-b">fab-b</a></div>
 									<div class="location">Nice, France</div>
 								</div>
+								<div class="member">
+									<h5>Benoit Charbonnier</h5>
+									<img src="https://1.gravatar.com/avatar/74b3575da062a5bdc1e47b7853897d55?d=https%3A%2F%2Fidenticons.github.com%2F21d1ac347eedb3fdb2f6850e1fa48e63.png&s=420" />
+									<div class="github"><a href="https://github.com/benouat">benouat</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>David-Emmanuel Divernois</h5>
+									<img src="https://1.gravatar.com/avatar/a7a83041f787ea36c9c3479d15f279b9?d=https%3A%2F%2Fidenticons.github.com%2F3745dd3cd5804ac5c82296aa14a15f6d.png&s=420" />
+									<div class="github"><a href="https://github.com/divdavem">divdavem</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>Jakub Gieryluk</h5>
+									<img src="https://0.gravatar.com/avatar/96246ecff3f790f6b8a35c80c6f67ea3?d=https%3A%2F%2Fidenticons.github.com%2Fa9e221eedf307e30b0435e5dfcc85b2c.png&s=420" />
+									<div class="github"><a href="https://github.com/jakub-g">jakub-g</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>Olaf Kappes</h5>
+									<img src="https://0.gravatar.com/avatar/c726c173ec756a07769e5a6b869047ac?d=https%3A%2F%2Fidenticons.github.com%2Fe977dc5360034ab9e81c4f4a4dc4f23e.png&s=420" />
+									<div class="github"><a href="https://github.com/olaf-k">olaf-k</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>Marc Laval</h5>
+									<img src="https://0.gravatar.com/avatar/966aefe4c53862b7cb79819c32075a9c?d=https%3A%2F%2Fidenticons.github.com%2F07787beafddb4b4da7135e30b209b2f9.png&s=420" />
+									<div class="github"><a href="https://github.com/mlaval">mlaval</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>Francesco Longo</h5>
+									<img src="https://1.gravatar.com/avatar/f045cf96a82ac30d9d92c663b4828e00?d=https%3A%2F%2Fidenticons.github.com%2F3305083bea1b77da40ce089b61d6bf5c.png&s=420" />
+									<div class="github"><a href="https://github.com/flongo">flongo</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>Srinath Madapathi</h5>
+									<img src="https://1.gravatar.com/avatar/daeef354905e477cd4ecca2d9eb87deb?d=https%3A%2F%2Fidenticons.github.com%2F93ae3dc8b1d5055029e16dde71bd2458.png&s=420" />
+									<div class="github"><a href="https://github.com/smadapathi">smadapathi</a></div>
+									<div class="location">Bangalore, India</div>
+								</div>
+								<div class="member">
+									<h5>Girish Manjunath</h5>
+									<img src="https://2.gravatar.com/avatar/cb7e0f1c2a70bd8fe7af432260cf982c?d=https%3A%2F%2Fidenticons.github.com%2F26887546162cf3a00952e14b3ec38269.png&s=420" />
+									<div class="github"><a href="https://github.com/Gprasad">Gprasad</a></div>
+									<div class="location">Bangalore, India</div>
+								</div>
+								<div class="member">
+									<h5>Yannick Meine</h5>
+									<img src="https://1.gravatar.com/avatar/381441499d2134da8f9d940898d99e6f?d=https%3A%2F%2Fidenticons.github.com%2Fbe04dbf99a923c527b93f664a489bca5.png&s=420" />
+									<div class="github"><a href="https://github.com/ymeine">ymeine</a></div>
+									<div class="location">Nice, France</div>
+								</div>
+								<div class="member">
+									<h5>Renju Richard</h5>
+									<img src="https://0.gravatar.com/avatar/144422d124027fc35f9d4be41db5eb85?d=https%3A%2F%2Fidenticons.github.com%2Fd4635f7b6b492241a6e76f7c2d5bbdb9.png&s=420" />
+									<div class="github"><a href="https://github.com/renjurichard">renjurichard</a></div>
+									<div class="location">Bangalore, India</div>
+								</div>
+								<div class="member">
+									<h5>Lorenzo Simone</h5>
+									<img src="https://2.gravatar.com/avatar/25d72b9112a53ce6f9893cca5cf10608?d=https%3A%2F%2Fidenticons.github.com%2F78a2de8195a0b6893c6fbb324a1a1ef9.png&s=420" />
+									<div class="github"><a href="https://github.com/lsimone">lsimone</a></div>
+									<div class="location">Nice, France</div>
+								</div>
 							</div>
-							
 							<div class="past-team">
-								<h3>Past developpers</h3>
+								<h3>Previous developpers</h3>
 								<p>People who significantly contributed in the past, but are no longer involved in Aria Templates development.</p>
 								<dl class="left">
 									<dt>Benoit Baliguet</dt>
 									<dd>Core development, performance tuning</dd>
+									<dt>Patrick Brosset</dt>
+									<dd>Core development lead</dd>
+									<dt>Federico Cagliero</dt>
+									<dd>Documentation, tool development</dd>
+									<dt>Anand Chandrabhanu</dt>
+									<dd>Core development</dd>
+									<dt>Krzysztof Cisło</dt>
+									<dd>Java tools development</dd>
+									<dt>Fabio Crisci</dt>
+									<dd>Core development, tech expert</dd>
+									<dt>Matthieu Dadou</dt>
+									<dd>Core development de passage</dd>
 									<dt>Julian Descottes</dt>
 									<dd>Core and unit test tool development</dd>
-									<dt>Federico Cagliero</dt>
-									<dd>Documentation tool development</dd>
-									<dt>Krzysztof Cislo</dt>
-									<dd>Java tools development</dd>
-									<dt>Richard Perkowski</dt>
-									<dd>Eclipse editor development</dd>
-									<dt>Antonio d'Ettole</dt>
-									<dd>Core development</dd>
 								</dl>
 								<dl class="left">
-									<dt>Jan Tarnawski</dt>
-									<dd>Development and support</dd>
+									<dt>Antonio d'Ettole</dt>
+									<dd>Core development</dd>
 									<dt>James Farrell</dt>
 									<dd>Development and support</dd>
+									<dt>Jineesh John</dt>
+									<dd>Core development</dd>
 									<dt>Dean Mustapha</dt>
+									<dd>Development and support</dd>
+									<dt>Richard Perkowski</dt>
+									<dd>Eclipse editor development</dd>
+									<dt>Vincent Renaudin</dt>
+									<dd>Core development</dd>
+									<dt>Jan Tarnawski</dt>
 									<dd>Development and support</dd>
 									<dt>Rickard Winberg</dt>
 									<dd>Development and support</dd>
-									<dt>Vincent Renaudin</dt>
-									<dd>Core development</dd>
-									
 								</dl>							
 							</div>
 							<div class="sponsors">
 								<h3>Amadeus sponsors</h3>
 								<dl>
-									<dt>Denis Lacroix <a class="linkedin" href="http://fr.linkedin.com/in/denislacroix">&nbsp;</a></dt>
-									<dd>Vice President</dd>
-									<dd>Sales &amp; eCommerce Platform</dd>
+									<dt>Denis Lacroix</dt>
+									<dd>Vice President R&D Reservation & Distribution</dd>
 									<dt>Bruno Chabrier</dt>
-									<dd>Director User Experience</dd>
-									<dd>Sales &amp; eCommerce Platform</dd>
+									<dd>Director Data Management & User Interface</dd>
 								</dl>
 							</div>
 							
@@ -157,20 +158,20 @@
 								<h3>Coordination and architecture</h3>
 								<dl>
 									<dt>Bertrand Laporte</dt>
-									<dd>Creator, unit manager</dd>
-									<dt>Patrick Brosset</dt>
+									<dd>Creator, senior manager</dd>
+									<dt>Marc Laval</dt>
 									<dd>Core development lead</dd>
 									<dt>Olaf Kappes</dt>
-									<dd>Documentation &amp; support lead</dd>
+									<dd>Architecture &amp; support lead</dd>
 								</dl>
 							</div>
 						</article>
 						<aside class="column sidebar">
 							<div>
-								<h5>Talk about us</h5>
+								<h5>We're on Twitter</h5>
 								<p>
-<a href="https://twitter.com/intent/tweet?button_hashtag=ariatemplates" class="twitter-hashtag-button" data-size="large" data-related="ariatemplates">Tweet #ariatemplates</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>								</p>
+									<a href="https://twitter.com/ariatemplates" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @ariatemplates</a>
+									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 							</div>
 							
 						</aside>
